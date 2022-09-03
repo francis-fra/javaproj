d8881385e561a9b86b4cb59cb47c713d1ea36de5
fra <fcnchan@yahoo.com>
2022-09-03 13:29:37 +1000

    init

---
+++ run.sh
@@ -0,0 +1,13 @@
+# project setup
+gradle init --type java-application
+
+# edit build.gradle (main class name & task)
+
+# run defualt
+gradle -q run
+
+# run task
+gradle -q execute_app
+
+# run test
+gradle test
